71495b8d570736e424bfedfda2136571e19df34f
Mauricio Monroy <mauricio.monroy0@soy.sena.edu.co>
2024-07-11 12:34:40 -0500

    Primer commit - Creación del proyecto con SpringBoot

---
+++ src/test/java/codelicht/sipressspringapp/SipressSpringAppApplicationTests.java
@@ -0,0 +1,13 @@
+package codelicht.sipressspringapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SipressSpringAppApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
