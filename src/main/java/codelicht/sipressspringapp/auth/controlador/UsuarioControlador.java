81d036ed67ed044a3681701d399bb9620a5ebeda
Mauricio Monroy <mauricio.monroy0@soy.sena.edu.co>
2024-08-15 16:38:04 -0500

    Se crean las clases, interfaces, métodos y demás elementos necesarios para la implementación del servicio de seguridad

---
+++ src/main/java/codelicht/sipressspringapp/auth/controlador/UsuarioControlador.java
@@ -0,0 +1,45 @@
+package codelicht.sipressspringapp.auth.controlador;
+
+import codelicht.sipressspringapp.auth.entidad.Usuario;
+import codelicht.sipressspringapp.auth.servicio.UserService;
+import org.springframework.http.ResponseEntity;
+import org.springframework.security.access.prepost.PreAuthorize;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+
+/**
+ * Controlador para la gestión de usuarios
+ * Proporciona métodos para la obtención de información de usuarios autenticados y de todos los usuarios
+ */
+@RequestMapping("/sipress-app/usuarios")
+@RestController
+public class UsuarioControlador {
+    private final UserService userService;
+
+    public UsuarioControlador(UserService userService) {
+        this.userService = userService;
+    }
+
+    @GetMapping("/perfil")
+    @PreAuthorize("isAuthenticated()")
+    public ResponseEntity<Usuario> authenticatedUser() {
+        Authentication authentication = SecurityContextHolder.getContext().getAuthentication();
+
+        Usuario usuarioActual = (Usuario) authentication.getPrincipal();
+
+        return ResponseEntity.ok(usuarioActual);
+    }
+
+    @GetMapping("/")
+    @PreAuthorize("hasAnyRole('ADMIN', 'SUPERADMIN')")
+    public ResponseEntity<List<Usuario>> allUsers() {
+        List<Usuario> usuarios = userService.allUsers();
+
+        return ResponseEntity.ok(usuarios);
+    }
+}
---
+++ src/main/java/codelicht/sipressspringapp/auth/entidad/RoleEnum.java
@@ -0,0 +1,10 @@
+package codelicht.sipressspringapp.auth.entidad;
+
+/**
+ * Enumeración de los roles que pueden tener los usuarios
+ */
+public enum RoleEnum {
+    USER,
+    ADMIN,
+    SUPERADMIN
+}
