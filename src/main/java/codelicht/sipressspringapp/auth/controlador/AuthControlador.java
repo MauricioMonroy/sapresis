648399ed35d6fb7f0b2840968f9eb30bc84f5084
Mauricio Monroy <mauricio.monroy0@soy.sena.edu.co>
2024-09-01 14:58:38 -0500

    Se añade método y función para el manejo del logout de la aplicación

--- src/main/java/codelicht/sipressspringapp/auth/controlador/AuthControlador.java
+++ src/main/java/codelicht/sipressspringapp/auth/controlador/AuthControlador.java
@@ -6,7 +6,12 @@ import codelicht.sipressspringapp.auth.dtos.RegistrarUsuarioDto;
 import codelicht.sipressspringapp.auth.entidad.Usuario;
 import codelicht.sipressspringapp.auth.servicio.AuthenticationService;
 import codelicht.sipressspringapp.auth.servicio.JwtService;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.security.web.authentication.logout.SecurityContextLogoutHandler;
 import org.springframework.web.bind.annotation.*;
 
 /**
@@ -43,4 +48,14 @@ public class AuthControlador {
 
         return ResponseEntity.ok(loginResponse);
     }
+
+    @PostMapping("/logout")
+    public ResponseEntity<Void> logout(HttpServletRequest request, HttpServletResponse response) {
+        Authentication auth = SecurityContextHolder.getContext().getAuthentication();
+        if (auth != null) {
+            new SecurityContextLogoutHandler().logout(request, response, auth);
+        }
+        return ResponseEntity.ok().build();
+    }
+
 }
