f55ef9fb1bd3e709aa79a8a1483633bc215f7669
Mauricio Monroy <mauricio.monroy0@soy.sena.edu.co>
2024-10-27 07:35:43 -0500

    Bloque de información adicional en el login

--- frontend/src/comunes/Login.js
+++ frontend/src/comunes/Login.js
@@ -149,6 +149,35 @@ export default function Login() {
                           </div>
                         </div>
                       </form>
+                      <div className="footer-info mt-4 text-center">
+                        <div className="social-icons">
+                          <a
+                            href="https://github.com/MauricioMonroy/sapresis.git"
+                            aria-label="Repositorio GitHub de Mauricio Monroy"
+                            rel="noopener noreferrer"
+                            target="_blank">
+                            <i className="fa-brands fa-github"></i>
+                          </a>
+                          <a
+                            href="https://call.whatsapp.com/voice/JqIwkDwqS2qpUUqbFjMwwd"
+                            aria-label="Contacto Whatsapp"
+                            rel="noopener noreferrer"
+                            target="_blank">
+                            <i className="fa-brands fa-whatsapp"></i>
+                          </a>
+                          <a
+                            href="https://t.me/+GVvMDQ4p8p84ZWJh"
+                            aria-label="Contacto Telegram"
+                            rel="noopener noreferrer"
+                            target="_blank">
+                            <i className="fa-brands fa-telegram"></i>
+                          </a>
+                        </div>
+                        <span>
+                          Copyright &copy; 2024, Codelicht Software Solutions.
+                        </span>
+                        <p>Todos los derechos reservados.</p>
+                      </div>
                     </div>
                   </div>
                 </div>
